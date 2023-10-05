5b963b517373de5c9cdbff242d9bed07a2f033c1
malam111 <0xmalam06@gmail.com>
2023-10-04 21:47:48 -0400

    ignore

---
+++ utils/battery/target/release/deps/battery-6d0be874a89cd4c9.d
@@ -0,0 +1,8 @@
+/home/malam/Code/apakabar/utils/battery/target/release/deps/battery-6d0be874a89cd4c9.rmeta: src/lib.rs src/battery.rs
+
+/home/malam/Code/apakabar/utils/battery/target/release/deps/libbattery-6d0be874a89cd4c9.rlib: src/lib.rs src/battery.rs
+
+/home/malam/Code/apakabar/utils/battery/target/release/deps/battery-6d0be874a89cd4c9.d: src/lib.rs src/battery.rs
+
+src/lib.rs:
+src/battery.rs:
---
+++ utils/battery/target/release/libbattery.d
@@ -0,0 +1,1 @@
+/home/malam/Code/apakabar/utils/battery/target/release/libbattery.rlib: /home/malam/Code/apakabar/utils/battery/src/battery.rs /home/malam/Code/apakabar/utils/battery/src/lib.rs
